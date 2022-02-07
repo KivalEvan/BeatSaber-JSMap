f69d239aa8fc4eed1695cf4de62ca431b3945360
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2022-02-07 23:19:40 +0800

    sample test

--- denosample.ts
+++ denosample.ts
@@ -8,12 +8,20 @@ bsmap.settings.mapDirectory =
     'D:/SteamLibrary/steamapps/common/Beat Saber/Beat Saber_Data/CustomWIPLevels/testmap/';
 
 const info = bsmap.load.infoSync('Info.dat');
-const difficulty = bsmap.load.difficultySync('map.dat');
+const difficulty = bsmap.load.difficultySync('ExpertPlusStandard.dat');
 
 info._previewDuration = 14;
 info._difficultyBeatmapSets.forEach((set) =>
     console.log(set._beatmapCharacteristicName, set._difficultyBeatmaps.length)
 );
+
+const BPM = bsmap.bpm.create(info._beatsPerMinute);
+const NJS = bsmap.njs.create(BPM, 16);
+NJS.offset = 0;
+
+console.log(BPM.toRealTime(42));
+console.log(BPM.toBeatTime(6.9));
+console.log(NJS.calcHalfJumpDuration(), NJS.calcHalfJumpDuration(0.5));
 
 difficulty._events.push({
     _time: 1,
@@ -21,3 +29,6 @@ difficulty._events.push({
     _value: 1,
     _floatValue: 1,
 });
+
+bsmap.save.infoSync('Info.dat', info);
+await bsmap.save.difficulty('ExpertPlusStandard.dat', difficulty);
