99bd7c51f7f0296387f4b7ac040e78c62ef60988
Kival <5645391+KivalEvan@users.noreply.github.com>
2022-06-01 16:13:22 +0800

    Spell check.

--- deno/mod.ts
+++ deno/mod.ts
@@ -1,6 +1,6 @@
 export * from './beatmap/mod.ts';
 export * as types from './types/mod.ts';
-export * as convert from './convertors/mod.ts';
+export * as convert from './converter/mod.ts';
 export * as load from './load.ts';
 export * as save from './save.ts';
 export * as optimize from './optimize.ts';
