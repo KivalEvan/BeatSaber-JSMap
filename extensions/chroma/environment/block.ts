5b78ff4a00af55aeccc386d49ec71e913a24b185
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2022-12-04 15:51:46 +0800

    Fix environment block copy

--- extensions/chroma/environment/block.ts
+++ extensions/chroma/environment/block.ts
@@ -1,53 +1,53 @@
 import { Vector3 } from '../../../types/vector.ts';
 import { IChromaEnvironment } from '../../../types/beatmap/v3/custom/chroma.ts';
 import { IChromaEnvironmentPlacement } from '../types/environment.ts';
+import { deepCopy } from '../../../utils/misc.ts';
 
 /** **IMPORTANT:** Manually adjust block to be exactly 1x1x1 unity unit (1x1x1 scale does usually not work) */
 export class EnvironmentBlock {
     data;
     anchor;
-    lightType?: number;
     static startLightID = 100;
     static index = 0;
-    protected constructor(data: IChromaEnvironment, anchor: Vector3, lightType?: number) {
+    protected constructor(data: IChromaEnvironment, anchor: Vector3) {
         this.data = data;
         this.anchor = anchor;
-        this.lightType = lightType;
     }
-    static create(data: IChromaEnvironment, anchor: Vector3, lightType?: number) {
-        return new this(data, anchor, lightType);
+    static create(data: IChromaEnvironment, anchor: Vector3) {
+        return new this(data, anchor);
     }
 
     place(options: IChromaEnvironmentPlacement, insertTo?: never): IChromaEnvironment;
     place(options: IChromaEnvironmentPlacement, insertTo: IChromaEnvironment[]): void;
     place(options: IChromaEnvironmentPlacement, insertTo?: IChromaEnvironment[]): IChromaEnvironment | void {
+        const d = deepCopy(this.data as {}) as IChromaEnvironment;
         const scale = options.scale
-            ? ((this.data.scale ? this.data.scale : [1, 1, 1]).map((s, i) => s * options.scale![i]) as Vector3)
-            : this.data.scale;
+            ? ((d.scale ? d.scale : [1, 1, 1]).map((s, i) => s * options.scale![i]) as Vector3)
+            : d.scale;
         const position = options.position
             ? (options.position.map((p, i) => p + this.anchor[i] * (options.scale?.[i] || 1)) as Vector3)
-            : ((this.data.position ? this.data.position : [0, 0, 0]).map(
+            : ((d.position ? d.position : [0, 0, 0]).map(
                 (p, i) => p + this.anchor[i] * (options.scale?.[i] ?? 1),
             ) as Vector3);
         const rotation = options.rotation
-            ? (options.rotation.map((r, i) => r + (this.data.rotation?.[i] ?? 0)) as Vector3)
-            : this.data.rotation;
+            ? (options.rotation.map((r, i) => r + (d.rotation?.[i] ?? 0)) as Vector3)
+            : d.rotation;
         const components = {
-            ...this.data.components,
+            ...d.components,
         };
-        if (typeof this.lightType === 'number') {
-            components.ILightWithId = {
-                type: this.lightType,
-                lightID: EnvironmentBlock.startLightID + EnvironmentBlock.index++,
-            };
+        if (typeof components.ILightWithId?.type === 'number') {
+            if (typeof options.type === 'number') components.ILightWithId.type = options.type;
+            components.ILightWithId.lightID = EnvironmentBlock.startLightID + EnvironmentBlock.index++;
         }
         const data = {
-            ...this.data,
+            ...d,
             scale,
             position,
             rotation,
-            components,
         };
+        if (Object.entries(components).length) {
+            data.components = components;
+        }
         if (insertTo) {
             insertTo.push(data);
             return;
