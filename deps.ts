2eb4070867f30274ca2534b47f0617b712e3b58d
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2023-05-30 07:40:15 -0700

    Change path dependency to mod

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
 export { dim, gray, red, yellow } from 'https://deno.land/std@0.185.0/fmt/colors.ts';
 export { exists, existsSync } from 'https://deno.land/std@0.185.0/fs/exists.ts';
-export { resolve } from 'https://deno.land/std@0.185.0/path/posix.ts';
+export { resolve } from 'https://deno.land/std@0.185.0/path/mod.ts';
