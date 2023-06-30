f51c37739dc56ad95b90733a502cff9b67095858
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2023-06-30 09:45:37 +0800

    Update patch info again

--- patch/dataCorrection/info.ts
+++ patch/dataCorrection/info.ts
@@ -1,15 +1,16 @@
-import { fixBoolean, fixFloat, fixString } from './helpers.ts';
+import { fixBoolean, fixFloat, fixInt, fixString } from './helpers.ts';
 import { IWrapInfo } from '../../types/beatmap/wrapper/info.ts';
 import { EnvironmentRename } from '../../beatmap/shared/environment.ts';
 import { IColor } from '../../types/colors.ts';
 import logger from '../../logger.ts';
+import { clamp } from '../../utils/math.ts';
 
-function fixEnvironment(str: unknown): IWrapInfo['environmentName'] {
+function fixEnvironment(str: unknown, all = false): IWrapInfo['environmentName'] {
    if (typeof str === 'string') {
       if (str === 'Origins') return 'OriginsEnvironment';
       if (
          Object.keys(EnvironmentRename)
-            .filter((env) => env !== 'GlassDesertEnvironment')
+            .filter((env) => all || env !== 'GlassDesertEnvironment')
             .includes(str)
       ) {
          return str as IWrapInfo['environmentName'];
@@ -18,16 +19,27 @@ function fixEnvironment(str: unknown): IWrapInfo['environmentName'] {
    return 'DefaultEnvironment';
 }
 
-function fixColorObject(val: unknown): Required<IColor> {
+function fixColorObject(val: unknown, req?: boolean): IColor;
+function fixColorObject(val: unknown, req: false): NonNullable<IColor>;
+function fixColorObject(val: unknown, req: true): Required<IColor>;
+function fixColorObject(val: unknown, req?: boolean) {
    if (typeof val === 'object' && !Array.isArray(val) && val) {
-      return {
-         r: fixFloat((val as IColor).r, 0),
-         g: fixFloat((val as IColor).g, 0),
-         b: fixFloat((val as IColor).b, 0),
-         a: fixFloat((val as IColor).a, 1),
-      };
+      if (req) {
+         return {
+            r: fixFloat((val as IColor).r, 0),
+            g: fixFloat((val as IColor).g, 0),
+            b: fixFloat((val as IColor).b, 0),
+            a: fixFloat((val as IColor).a, 1),
+         };
+      } else {
+         return {
+            r: fixFloat((val as IColor).r, 0),
+            g: fixFloat((val as IColor).g, 0),
+            b: fixFloat((val as IColor).b, 0),
+         };
+      }
    }
-   return { r: 0, g: 0, b: 0, a: 1 };
+   return req ? { r: 0, g: 0, b: 0, a: 1 } : { r: 0, g: 0, b: 0 };
 }
 
 export function info(data: IWrapInfo) {
@@ -52,23 +64,36 @@ export function info(data: IWrapInfo) {
    data.environmentName = fixEnvironment(data.environmentName);
    data.allDirectionsEnvironmentName = 'GlassDesertEnvironment';
    if (Array.isArray(data.environmentNames)) {
-      data.environmentNames.forEach((v) => fixEnvironment(v));
+      data.environmentNames = data.environmentNames
+         .filter((v) => v)
+         .map((v) => fixEnvironment(v, true));
    } else data.environmentNames = [];
    if (Array.isArray(data.colorSchemes)) {
-      data.colorSchemes.forEach((v) => {
-         v.useOverride = fixBoolean(v.useOverride);
-         v.colorScheme.name = fixString(v.colorScheme.name, 'Unknown');
-         v.colorScheme.saberLeftColor = fixColorObject(v.colorScheme.saberLeftColor);
-         v.colorScheme.saberRightColor = fixColorObject(v.colorScheme.saberRightColor);
-         v.colorScheme.environment0Color = fixColorObject(v.colorScheme.environment0Color);
-         v.colorScheme.environment1Color = fixColorObject(v.colorScheme.environment1Color);
-         v.colorScheme.obstaclesColor = fixColorObject(v.colorScheme.obstaclesColor);
-         v.colorScheme.environment0ColorBoost = fixColorObject(
-            v.colorScheme.environment0ColorBoost,
-         );
-         v.colorScheme.environment1ColorBoost = fixColorObject(
-            v.colorScheme.environment1ColorBoost,
-         );
-      });
+      data.colorSchemes = data.colorSchemes
+         .filter((v) => v)
+         .map((v) => {
+            v.useOverride = fixBoolean(v.useOverride);
+            v.colorScheme.name = fixString(v.colorScheme.name, 'Unknown');
+            v.colorScheme.saberLeftColor = fixColorObject(v.colorScheme.saberLeftColor, true);
+            v.colorScheme.saberRightColor = fixColorObject(v.colorScheme.saberRightColor, true);
+            v.colorScheme.environment0Color = fixColorObject(v.colorScheme.environment0Color, true);
+            v.colorScheme.environment1Color = fixColorObject(v.colorScheme.environment1Color, true);
+            v.colorScheme.obstaclesColor = fixColorObject(v.colorScheme.obstaclesColor, true);
+            v.colorScheme.environment0ColorBoost = fixColorObject(
+               v.colorScheme.environment0ColorBoost,
+               true,
+            );
+            v.colorScheme.environment1ColorBoost = fixColorObject(
+               v.colorScheme.environment1ColorBoost,
+               true,
+            );
+            return v;
+         });
    } else data.colorSchemes = [];
+   for (const [_, d] of data.listMap()) {
+      d.colorSchemeId = clamp(fixInt(d.colorSchemeId, 0), 0, data.environmentNames.length - 1);
+      d.environmentId = clamp(fixInt(d.environmentId, 0), 0, data.colorSchemes.length - 1);
+      d.njs = fixFloat(d.njs, 0);
+      d.njsOffset = fixFloat(d.njsOffset, 0);
+   }
 }
