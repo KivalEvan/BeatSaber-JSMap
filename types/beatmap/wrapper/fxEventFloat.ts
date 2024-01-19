3baa41ba9d552151aa0091ee7a31bcc44c21464d
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-01-20 04:25:43 +0800

    somehow missed this

--- types/beatmap/wrapper/fxEventFloat.ts
+++ types/beatmap/wrapper/fxEventFloat.ts
@@ -6,17 +6,8 @@ export interface IWrapFxEventFloatAttribute<
 > extends IWrapBaseObjectAttribute<T> {
    /** Relative beat time `<float>` to event box group. */
    time: number;
-   /**
-    * Ease type `<int>` of FX event.
-    * ```ts
-    * -1 -> Step
-    * 0 -> Linear
-    * 1 -> EaseInQuad
-    * 2 -> EaseOutQuad
-    * 3 -> EaseInOutQuad
-    * ```
-    */
-   easing: -1 | 0 | 1 | 2 | 3;
+   /** Ease type `<int>` of FX event. */
+   easing: number;
    /** Use previous event value `<int>` in FX event. */
    previous: 0 | 1;
    /** Value `<float>` of FX event. */
@@ -26,6 +17,6 @@ export interface IWrapFxEventFloatAttribute<
 export interface IWrapFxEventFloat<T extends { [P in keyof T]: T[P] } = Record<string, any>>
    extends IWrapBaseObject<T>, IWrapFxEventFloatAttribute<T> {
    setPrevious(value: 0 | 1): this;
-   setEasing(value: -1 | 0 | 1 | 2 | 3): this;
+   setEasing(value: number): this;
    setValue(value: number): this;
 }
