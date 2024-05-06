bc0620a6a9c3dd49093851e5520ca6dcb31f15ed
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-05-06 19:11:56 +0800

    Version utils update

--- beatmap/shared/version.ts
+++ beatmap/shared/version.ts
@@ -1,23 +1,33 @@
 import type { Version } from '../../types/beatmap/shared/version.ts';
 
+export function retrieveVersion(json: Record<string, unknown>): Version | null {
+   const ver = json._version ?? json.version;
+   if (typeof ver !== 'string') {
+      return null;
+   }
+   return ver as unknown as Version;
+}
+
 /** Internal use, compare beatmap version to another. */
-export function compareVersion(version: Version, compareTo: Version): 'old' | 'new' | 'current' {
-   const ver = getVersionArray(version);
-   const verCompare = getVersionArray(compareTo);
-   for (const num in ver) {
-      if (ver[num] < verCompare[num]) {
-         return 'old';
+export function compareVersion(
+   current: Version,
+   compareTo: Version,
+): -1 | 0 | 1 {
+   const verCurrent = getVersionArray(current);
+   const verCompareTo = getVersionArray(compareTo);
+   for (const num in verCurrent) {
+      if (verCurrent[num] < verCompareTo[num]) {
+         return -1;
       }
-      if (ver[num] > verCompare[num]) {
-         return 'new';
+      if (verCurrent[num] > verCompareTo[num]) {
+         return 1;
       }
    }
-   return 'current';
+   return 0;
 }
 
 function getVersionArray(version: Version): number[] {
    return version
-      .replace('v', '')
       .split('.')
       .map((el) => parseInt(el));
 }
