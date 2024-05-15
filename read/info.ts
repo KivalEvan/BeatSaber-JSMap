56b58cefde550e6b9fbb5c5cdfa0b84fb45a3686
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-05-16 00:15:13 +0800

    Tweak comment

--- read/info.ts
+++ read/info.ts
@@ -1,4 +1,3 @@
-import type { GenericFilename } from '../types/beatmap/shared/filename.ts';
 import logger from '../logger.ts';
 import type { LooseAutocomplete } from '../types/utils.ts';
 import type { IWrapInfo } from '../types/beatmap/wrapper/info.ts';
@@ -7,23 +6,27 @@ import { handleRead, handleReadSync, tag } from './_main.ts';
 
 /**
  * Asynchronously read beatmap info file.
+ *
+ * @example
  * ```ts
  * readInfoFile('Info.dat', 4).then((data) => console.log(data));
  * ```
  *
+ * Default to `Info.dat` if path not provided.
+ *
  * Mismatched beatmap version will be automatically converted, unspecified will leave the version as is but not known.
  */
 export function readInfoFile(
-   path?: LooseAutocomplete<GenericFilename>,
+   path?: LooseAutocomplete<'Info.dat' | 'info.dat'>,
    version?: number | null,
    options?: IReadOptions<IWrapInfo>,
 ): Promise<IWrapInfo>;
 export function readInfoFile(
-   path?: LooseAutocomplete<GenericFilename>,
+   path?: LooseAutocomplete<'Info.dat' | 'info.dat'>,
    options?: IReadOptions<IWrapInfo>,
 ): Promise<IWrapInfo>;
 export function readInfoFile(
-   path: LooseAutocomplete<GenericFilename> = 'Info.dat',
+   path: LooseAutocomplete<'Info.dat' | 'info.dat'> = 'Info.dat',
    version?: number | null | IReadOptions<IWrapInfo>,
    options?: IReadOptions<IWrapInfo>,
 ): Promise<IWrapInfo> {
@@ -33,24 +36,28 @@ export function readInfoFile(
 
 /**
  * Synchronously read beatmap info file.
+ *
+ * @example
  * ```ts
  * const info = readInfoFileSync('Info.dat', 4);
  * console.log(info);
  * ```
  *
+ * Default to `Info.dat` if path not provided.
+ *
  * Mismatched beatmap version will be automatically converted, unspecified will leave the version as is but not known.
  */
 export function readInfoFileSync(
-   path?: LooseAutocomplete<GenericFilename>,
+   path?: LooseAutocomplete<'Info.dat' | 'info.dat'>,
    version?: number | null,
    options?: IReadOptions<IWrapInfo>,
 ): IWrapInfo;
 export function readInfoFileSync(
-   path?: LooseAutocomplete<GenericFilename>,
+   path?: LooseAutocomplete<'Info.dat' | 'info.dat'>,
    options?: IReadOptions<IWrapInfo>,
 ): IWrapInfo;
 export function readInfoFileSync(
-   path: LooseAutocomplete<GenericFilename> = 'Info.dat',
+   path: LooseAutocomplete<'Info.dat' | 'info.dat'> = 'Info.dat',
    version?: number | null | IReadOptions<IWrapInfo>,
    options?: IReadOptions<IWrapInfo>,
 ): IWrapInfo {
