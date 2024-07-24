640c33ebe16bd0ff8e23dd343d2a27098a2693cd
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-07-24 18:18:13 +0800

    Clarify time utils name

--- tests/utils/time.test.ts
+++ tests/utils/time.test.ts
@@ -2,37 +2,37 @@ import * as time from '../../utils/time.ts';
 import { assertEquals } from '../deps.ts';
 
 Deno.test('Second to MMSS string', () => {
-   assertEquals(time.toMmss(60), '1:00');
-   assertEquals(time.toMmss(30.5), '0:30');
-   assertEquals(time.toMmss(101), '1:41');
-   assertEquals(time.toMmss(-0), '0:00');
-   assertEquals(time.toMmss(-5), '-0:05');
-   assertEquals(time.toMmss(-5.25), '-0:06');
+   assertEquals(time.secToMmss(60), '1:00');
+   assertEquals(time.secToMmss(30.5), '0:30');
+   assertEquals(time.secToMmss(101), '1:41');
+   assertEquals(time.secToMmss(-0), '0:00');
+   assertEquals(time.secToMmss(-5), '-0:05');
+   assertEquals(time.secToMmss(-5.25), '-0:06');
 });
 
 Deno.test('Second to MMSSMS string', () => {
-   assertEquals(time.toMmssms(60), '1:00.000');
-   assertEquals(time.toMmssms(30.5153), '0:30.515');
-   assertEquals(time.toMmssms(101), '1:41.000');
-   assertEquals(time.toMmssms(-0), '0:00.000');
-   assertEquals(time.toMmssms(-5), '-0:05.000');
-   assertEquals(time.toMmssms(-5.25), '-0:05.250');
+   assertEquals(time.secToMmssms(60), '1:00.000');
+   assertEquals(time.secToMmssms(30.5153), '0:30.515');
+   assertEquals(time.secToMmssms(101), '1:41.000');
+   assertEquals(time.secToMmssms(-0), '0:00.000');
+   assertEquals(time.secToMmssms(-5), '-0:05.000');
+   assertEquals(time.secToMmssms(-5.25), '-0:05.250');
 });
 
 Deno.test('Minute to HHMMSS string', () => {
-   assertEquals(time.toHhmmss(60), '1:00:00');
-   assertEquals(time.toHhmmss(30.5), '0:30:30');
-   assertEquals(time.toHhmmss(101), '1:41:00');
-   assertEquals(time.toHhmmss(-0), '0:00:00');
-   assertEquals(time.toHhmmss(-5), '-0:05:00');
-   assertEquals(time.toHhmmss(-5.25), '-0:05:15');
+   assertEquals(time.minToHhmmss(60), '1:00:00');
+   assertEquals(time.minToHhmmss(30.5), '0:30:30');
+   assertEquals(time.minToHhmmss(101), '1:41:00');
+   assertEquals(time.minToHhmmss(-0), '0:00:00');
+   assertEquals(time.minToHhmmss(-5), '-0:05:00');
+   assertEquals(time.minToHhmmss(-5.25), '-0:05:15');
 });
 
 Deno.test('MMSS string to Second', () => {
-   assertEquals(time.mmssToFloat('1:00.000'), 60);
-   assertEquals(time.mmssToFloat('0:30.515'), 30);
-   assertEquals(time.mmssToFloat('  1:41.  000'), 101);
-   assertEquals(time.mmssToFloat('0:00.000'), 0);
-   assertEquals(time.mmssToFloat('-0:05.000'), -5);
-   assertEquals(time.mmssToFloat('   -0:05.250  '), -5);
+   assertEquals(time.mmssToSec('1:00.000'), 60);
+   assertEquals(time.mmssToSec('0:30.515'), 30);
+   assertEquals(time.mmssToSec('  1:41.  000'), 101);
+   assertEquals(time.mmssToSec('0:00.000'), 0);
+   assertEquals(time.mmssToSec('-0:05.000'), -5);
+   assertEquals(time.mmssToSec('   -0:05.250  '), -5);
 });
--- utils/time.ts
+++ utils/time.ts
@@ -1,7 +1,7 @@
 /**
  * Convert number value of second to MM:SS string.
  */
-export function toMmss(seconds: number): string {
+export function secToMmss(seconds: number): string {
    if (!seconds) {
       return '0:00';
    }
@@ -17,31 +17,31 @@ export function toMmss(seconds: number): string {
 /**
  * Convert number value of minute to HH:MM:SS string.
  */
-export function toHhmmss(minutes: number): string {
+export function minToHhmmss(minutes: number): string {
    if (!minutes) {
       return '0:00:00';
    }
    const sec = Math.abs(Math.round((minutes * 60) % 60))
       .toString()
       .padStart(2, '0');
-   return `${toMmss(minutes > 0 ? Math.floor(minutes) : Math.ceil(minutes))}:${sec}`;
+   return `${secToMmss(minutes > 0 ? Math.floor(minutes) : Math.ceil(minutes))}:${sec}`;
 }
 
 /**
  * Convert number value of second to MM:SS.SSS string.
  */
-export function toMmssms(seconds: number): string {
+export function secToMmssms(seconds: number): string {
    if (!seconds) {
       return '0:00.000';
    }
    const dec = (seconds % 1).toString().split('.')[1]?.padEnd(3, '0').slice(0, 3) || '000';
-   return `${toMmss(seconds > 0 ? Math.floor(seconds) : Math.ceil(seconds))}.${dec}`;
+   return `${secToMmss(seconds > 0 ? Math.floor(seconds) : Math.ceil(seconds))}.${dec}`;
 }
 
 /**
  * Convert MM:SS string to number value of second.
  */
-export function mmssToFloat(mmss: string): number {
+export function mmssToSec(mmss: string): number {
    const [m, s] = mmss.split(':').map((el) => Math.abs(parseInt(el)));
    const sign = mmss.trim().startsWith('-');
    return (sign ? -1 : 1) * (m * 60 + s);
