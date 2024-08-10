20ba1781174047b8049c59397448bec03c1cd91d
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2024-08-10 19:10:18 +0800

    Update

--- tests/globals.test.ts
+++ tests/globals.test.ts
@@ -8,29 +8,3 @@ Deno.test('Set global directory', () => {
    globals.directory = './start';
    assertEquals(globals.directory, path.resolve('./start/'));
 });
-
-Deno.test('Set global log level', () => {
-   globals.logLevel = 0;
-   assertEquals(globals.logLevel, 0);
-
-   globals.logLevel = 1;
-   assertEquals(globals.logLevel, 1);
-
-   globals.logLevel = 2;
-   assertEquals(globals.logLevel, 2);
-
-   globals.logLevel = 3;
-   assertEquals(globals.logLevel, 3);
-
-   globals.logLevel = 4;
-   assertEquals(globals.logLevel, 4);
-
-   globals.logLevel = 5;
-   assertEquals(globals.logLevel, 5);
-
-   globals.logLevel = -1;
-   assertEquals(globals.logLevel, 0);
-
-   globals.logLevel = 6;
-   assertEquals(globals.logLevel, 5);
-});
