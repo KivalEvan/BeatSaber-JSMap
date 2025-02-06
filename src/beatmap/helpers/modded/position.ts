47b8437d862c123732a432fbc4800df1142f576c
officialMECH <officialMECH@gmail.com>
2025-02-05 18:16:34 -0600

    clean up & bugfixes

--- src/beatmap/helpers/modded/position.ts
+++ src/beatmap/helpers/modded/position.ts
@@ -6,7 +6,7 @@ import type { Vector2 } from '../../../types/vector.ts';
  * Get position of Noodle Extensions in beatmap v2.
  */
 export function getNoodleExtensionsPositionV2<
-   T extends { customData: ICustomDataNote },
+   T extends { customData: Pick<ICustomDataNote, '_position'> },
 >(data: T): Vector2 | null {
    return Array.isArray(data.customData._position)
       ? [data.customData._position[0], data.customData._position[1]]
@@ -17,7 +17,7 @@ export function getNoodleExtensionsPositionV2<
  * Get position of Noodle Extensions in beatmap v3.
  */
 export function getNoodleExtensionsPositionV3<
-   T extends { customData: ICustomDataNote },
+   T extends { customData: Pick<ICustomDataNote, 'coordinates'> },
 >(data: T): Vector2 | null {
    return Array.isArray(data.customData.coordinates)
       ? [data.customData.coordinates[0], data.customData.coordinates[1]]
@@ -28,7 +28,7 @@ export function getNoodleExtensionsPositionV3<
  * Get tail position of Noodle Extensions in beatmap v3.
  */
 export function getNoodleExtensionsTailPositionV3<
-   T extends { customData: ICustomDataSlider },
+   T extends { customData: Pick<ICustomDataSlider, 'tailCoordinates'> },
 >(data: T): Vector2 | null {
    return Array.isArray(data.customData.tailCoordinates)
       ? [data.customData.tailCoordinates[0], data.customData.tailCoordinates[1]]
