86a7e8adc58fc697d5521dd5b1d413c2fe3dfba0
Kival Evan <5645391+KivalEvan@users.noreply.github.com>
2025-07-02 18:21:23 +0800

    Fix NE track helper

--- src/extensions/heck/noodleExtensions/track.ts
+++ src/extensions/heck/noodleExtensions/track.ts
@@ -1,72 +1,104 @@
 import type { INEObject } from './types/object.ts';
 
+function _addTrack(
+   currentTrack?: string | (string | null | undefined)[] | null,
+   trackToAdd?: string | string[] | null,
+): string | string[] | undefined {
+   if (!trackToAdd) {
+      return typeof currentTrack === 'string'
+         ? currentTrack
+         : Array.isArray(currentTrack)
+         ? (currentTrack.filter((s) => s) as string[])
+         : undefined;
+   }
+   if (typeof trackToAdd === 'string') {
+      return _addTrack(currentTrack, [trackToAdd]);
+   }
+   if (typeof currentTrack === 'string') {
+      return [currentTrack, ...trackToAdd];
+   }
+   if (Array.isArray(currentTrack)) {
+      currentTrack.push(...trackToAdd);
+      return currentTrack.filter((s) => s) as string[];
+   }
+   return [...trackToAdd];
+}
+
+function _removeTrack(
+   currentTrack?: string | (string | null | undefined)[] | null,
+   trackToRemove?: string | string[] | null,
+): string | string[] | undefined {
+   if (!trackToRemove || !currentTrack) {
+      return typeof currentTrack === 'string'
+         ? currentTrack
+         : Array.isArray(currentTrack)
+         ? (currentTrack.filter((s) => s) as string[])
+         : undefined;
+   }
+   if (typeof currentTrack === 'string') {
+      return currentTrack === trackToRemove ? undefined : currentTrack;
+   }
+   if (typeof trackToRemove === 'string') {
+      return _removeTrack(currentTrack, [trackToRemove]);
+   }
+   for (const track of trackToRemove) {
+      currentTrack.splice(currentTrack.indexOf(track), 1);
+   }
+   return currentTrack.length ? (currentTrack.filter((s) => s) as string[]) : undefined;
+}
+
 /**
  * Add track(s) to object(s).
  */
-export function addTrack<
-   T extends Pick<INEObject, 'customData'>,
->(objects: T[], track: string | string[]): void;
-export function addTrack<
-   T extends Pick<INEObject, 'customData'>,
->(object: T, track: string | string[]): void;
-export function addTrack<
-   T extends Pick<INEObject, 'customData'>,
->(objects: T | T[], track: string | string[]): void {
+export function addTrack<T extends { customData?: INEObject['customData'] }>(
+   objects: T[],
+   track: string | string[] | undefined,
+): T[];
+export function addTrack<T extends { customData?: INEObject['customData'] }>(
+   object: T,
+   track: string | string[] | undefined,
+): T;
+export function addTrack<T extends { customData?: INEObject['customData'] }>(
+   objects: T | T[],
+   track: string | string[] | undefined,
+): T | T[] {
    if (!Array.isArray(objects)) {
-      if (typeof track === 'string') {
-         if (!objects.customData.track) {
-            objects.customData.track = track;
-         } else if (Array.isArray(objects.customData.track)) {
-            if (!objects.customData.track.includes(track)) {
-               objects.customData.track.push(track);
-            }
-         } else if (objects.customData.track !== track) {
-            objects.customData.track = [objects.customData.track, track];
-         }
-      } else {
-         track.forEach((t) => addTrack(objects, t));
-      }
-      return;
+      objects.customData ||= {};
+      objects.customData.track = _addTrack(objects.customData.track, track);
+   } else {
+      objects.forEach((obj) => {
+         addTrack(obj, track);
+      });
    }
-   objects.forEach((obj) => {
-      addTrack(obj, track);
-   });
+   return objects;
 }
 
 /**
  * Remove track(s) from object(s).
  */
-export function removeTrack<
-   T extends Pick<INEObject, 'customData'>,
->(objects: T[], track: string | string[]): void;
-export function removeTrack<
-   T extends Pick<INEObject, 'customData'>,
->(object: T, track: string | string[]): void;
-export function removeTrack<
-   T extends Pick<INEObject, 'customData'>,
->(objects: T | T[], track: string | string[]): void {
+export function removeTrack<T extends { customData?: INEObject['customData'] }>(
+   objects: T[],
+   track: string | string[] | undefined,
+): T[];
+export function removeTrack<T extends { customData?: INEObject['customData'] }>(
+   object: T,
+   track: string | string[] | undefined,
+): T;
+export function removeTrack<T extends { customData?: INEObject['customData'] }>(
+   objects: T | T[],
+   track: string | string[] | undefined,
+): T | T[] {
    if (!Array.isArray(objects)) {
-      if (typeof track === 'string') {
-         if (!objects.customData.track) {
-            return;
-         } else if (Array.isArray(objects.customData.track)) {
-            if (objects.customData.track.includes(track)) {
-               objects.customData.track = objects.customData.track.filter(
-                  (t: string) => t !== track,
-               );
-               if (objects.customData.track.length === 1) {
-                  objects.customData.track = objects.customData.track[0];
-               }
-            }
-         } else if (objects.customData.track === track) {
-            delete objects.customData.track;
-         }
-      } else {
-         track.forEach((t) => removeTrack(objects, t));
+      if (objects.customData) {
+         objects.customData.track = _removeTrack(
+            objects.customData.track,
+            track,
+         );
       }
-      return;
+   } else {
+      objects.forEach((obj) => {
+         removeTrack(obj, track);
+      });
    }
-   objects.forEach((obj) => {
-      removeTrack(obj, track);
-   });
+   return objects;
 }
